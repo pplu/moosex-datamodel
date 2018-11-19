28fdc2b488438007fdd5d2e1d70ab15e5152e2eb
Jose Luis Martinez <jlmartinez@capside.com>
2018-11-19 22:24:55 +0100

    Bump version to 1.01

--- lib/MooseX/DataModel.pm
+++ lib/MooseX/DataModel.pm
@@ -2,7 +2,7 @@ package MooseX::DataModel {
   use Moose;
   use Moose::Exporter;
   use Moose::Util::TypeConstraints qw/find_type_constraint register_type_constraint coerce subtype from via/;
-  our $VERSION = "1.00";
+  our $VERSION = "1.01";
 
   Moose::Exporter->setup_import_methods(
     with_meta => [ qw/ key array object / ],
